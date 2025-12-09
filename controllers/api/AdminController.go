bd993e900c3a898da2fb143ec86bf9e1e4e7e14d
Fahrezi Banu Pradana <fahrezib.intern@ad.balitower.co.id>
2025-12-09 16:20:03 +0700

    test

---
+++ controllers/api/AdminController.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zxcas321/ProfileGolang/models"
+	"github.com/zxcas321/ProfileGolang/requests"
+	"github.com/zxcas321/ProfileGolang/services"
+	"github.com/zxcas321/ProfileGolang/utils"
+)
+
+func CreateAdmin(c *gin.Context){
+	
+}
